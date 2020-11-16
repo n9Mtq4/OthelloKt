32fc2bf7da4983118036a6dbdd2af24059c8034c
n9Mtq4 <bill.wresnahan@gmail.com>
2020-11-15 23:28:21 -0500

    Play an entire game with AB

---
+++ src/main/kotlin/com/n9mtq4/othellokt/AlphaBetaPlay.kt
@@ -0,0 +1,68 @@
+package com.n9mtq4.othellokt
+
+import clojure.lang.IFn
+
+/**
+ * Created by will on 11/15/20 at 11:14 PM.
+ *
+ * @author Will "n9Mtq4" Bresnahan
+ */
+
+
+/**
+ * Gets the best possible move for a given board using alpha beta
+ *
+ * @param heuristic the heuristic to pass to alpha beta
+ * @param state the board position to evaluate
+ * @param depth the depth to run alpha beta
+ * @return the best move determined by alpha beta using [heuristic]
+ * */
+fun abBestMove(heuristic: IFn, state: OthelloState, depth: Int): OthelloMove {
+	
+	// set up moves and player
+	val availableMoves = state.availableMoves()
+	val rev = state.current == 1
+	
+	// find the best move
+	var bestMove = availableMoves[0]
+	var bestMoveScore = Double.NEGATIVE_INFINITY
+	
+	for (move in availableMoves) {
+		
+		val score = state.current * alphaBetaKtCljFunc(heuristic, state.applyMove(move), depth - 1, !rev)
+		
+		if (score > bestMoveScore) {
+			bestMoveScore = score
+			bestMove = move
+		}
+		
+	}
+	
+	return bestMove
+	
+}
+
+/**
+ * Plays a game of Othello against two clojure heuristics. Uses
+ * alpha beta with a depth of [depth]
+ *
+ * @param black the black clojure heuristic
+ * @param white the white clojure heuristic
+ * @param depth the alpha beta depth
+ * @return the winner of the game
+ * */
+fun abPlayGame(black: IFn, white: IFn, depth: Int): Int {
+	
+	var board = OthelloState()
+	
+	while (!board.gameOver()) {
+		
+		val player = if (board.current == 1) black else white
+		
+		board = board.applyMove(abBestMove(player, board, depth))
+		
+	}
+	
+	return board.winner()
+	
+}
